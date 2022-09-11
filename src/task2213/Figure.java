4bb6cd0e5cd992e097437bdb34cbce6d14571316
Godri5 <godri5@yandex.ru>
2022-09-11 13:14:48 +0300

    Initialised Figure

--- src/task2213/Figure.java
+++ src/task2213/Figure.java
@@ -1,4 +1,25 @@
 package task2213;
 
 public class Figure {
+    private int x;
+    private int y;
+    private int[][] matrix = new int[3][3];
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
