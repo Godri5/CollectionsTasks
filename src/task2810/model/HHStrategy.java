8566c45e3212b97b33088cd935fb18d40347312a
Godri5 <godri5@yandex.ru>
2022-10-18 21:17:13 +0300

    Initial for task 2810

---
+++ src/task2810/model/HHStrategy.java
@@ -0,0 +1,27 @@
+package task2810.model;
+
+import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HHStrategy implements Strategy{
+    private static final String URL_FORMAT = "https://hh.ru/search/vacancy?text=java+%s&page=%d";
+
+    @Override
+    public List<Vacancy> getVacancies(String searchString) {
+        try {
+            Document doc = Jsoup.connect(URL_FORMAT)
+                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15")
+                    .referrer("https://hh.ru/")
+                    .get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ArrayList<>();
+    }
+}
