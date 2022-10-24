b24e62e12cee974653081e08fea2c3197704964e
Godri5 <godri5@yandex.ru>
2022-10-24 18:11:09 +0300

    Done two more steps in 2613

--- src/task2613/ConsoleHelper.java
+++ src/task2613/ConsoleHelper.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 
 public class ConsoleHelper {
     private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+    private static Operation operation;
 
     public static void writeMessage(String message) {
         System.out.println(message);
@@ -19,5 +20,57 @@ public class ConsoleHelper {
             e.printStackTrace();
         }
         return result;
+    }
+
+    public static String askCurrencyCode() {
+        writeMessage("Enter currency code:");
+        String currencyCode;
+        while (true) {
+            currencyCode = readString();
+            if (currencyCode == null || currencyCode.trim().length() != 3) {
+                writeMessage("Enter currency code once again");
+                continue;
+            }
+            return currencyCode.trim().toUpperCase();
+        }
+    }
+
+    public static String[] getValidTwoDigits(String currencyCode) {
+        writeMessage("Enter face value and number of banknotes");
+        while (true) {
+            String s = ConsoleHelper.readString();
+            String[] split = null;
+            if (s == null || (split = s.split(" ")).length != 2) {
+                writeMessage("Enter face value and number of banknotes again");
+            } else {
+                try {
+                    if (Integer.parseInt(split[0]) <= 0 || Integer.parseInt(split[1]) <= 0)
+                        writeMessage("Specify valid data.");
+                } catch (NumberFormatException e) {
+                    writeMessage("Specify valid data.");
+                    continue;
+                }
+                return split;
+            }
+        }
+    }
+
+    public static Operation askOperation() {
+        writeMessage("Enter number of operation you want to commit");
+        int operationCode;
+        try {
+            while (true) {
+                if ((operationCode = Integer.parseInt(bis.readLine())) > 4) {
+                    writeMessage("Specify valid data");
+                    continue;
+                } else {
+                    operation = operation.getAllowableOperationByOrdinal(operationCode);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return operation;
     }
 }
--- src/task2613/CurrencyManipulator.java
+++ src/task2613/CurrencyManipulator.java
@@ -1,6 +1,7 @@
 package task2613;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CurrencyManipulator {
     private String currencyCode;
@@ -8,9 +9,26 @@ public class CurrencyManipulator {
 
     public CurrencyManipulator(String currencyCode) {
         this.currencyCode = currencyCode;
+        this.denominations = new TreeMap<>();
     }
 
     public String getCurrencyCode() {
         return currencyCode;
+    }
+
+    public void addAmount(int denomination, int count) {
+        if (denominations.containsKey(denomination)){
+            denominations.put(denomination, denominations.get(denomination) + count);
+        } else {
+            denominations.put(denomination, count);
+        }
+    }
+
+    public int getTotalAmount() {
+        int result = 0;
+        for (Map.Entry cash : denominations.entrySet()) {
+            result += ((int) cash.getKey()) * ((int) cash.getValue());
+        }
+        return result;
     }
 }
